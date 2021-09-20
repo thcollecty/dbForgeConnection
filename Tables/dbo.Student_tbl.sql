3b2feed32d22bed79a89a4f069f0d6f3409cabba
Bilal Laeeq <bilal.laeeq@greenlightlabs.tech>
2021-09-20 12:11:41 +0500

    bilal

---
+++ Tables/dbo.Student_tbl.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Student_tbl] (
+  [StudentID] [int] IDENTITY,
+  [Name] [nvarchar](50) NOT NULL,
+  [Address] [nvarchar](50) NOT NULL,
+  [Email] [nvarchar](50) NOT NULL,
+  CONSTRAINT [PK_Student_tbl] PRIMARY KEY CLUSTERED ([StudentID])
+)
+ON [PRIMARY]
+GO
---
+++ Tables/dbo.Teacher_tbl.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Teacher_tbl] (
+  [TeacherID] [int] IDENTITY,
+  [Name] [nvarchar](50) NOT NULL,
+  [Address] [nvarchar](50) NOT NULL,
+  [Phone] [nvarchar](50) NOT NULL,
+  CONSTRAINT [PK_Teacher_tbl] PRIMARY KEY CLUSTERED ([TeacherID])
+)
+ON [PRIMARY]
+GO
