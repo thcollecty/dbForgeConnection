e94a85cf1060bb2fec5aa736e4b3d5d1068324b0
Bilal Laeeq <bilal.laeeq@greenlightlabs.tech>
2021-09-20 13:09:28 +0500

    Daniyal

---
+++ Tables/dbo.Department_tbl.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Department_tbl] (
+  [Department_ID] [int] IDENTITY,
+  [Department_Name] [nvarchar](50) NOT NULL,
+  [Department_Contact] [nvarchar](50) NOT NULL,
+  CONSTRAINT [PK_Department_tbl] PRIMARY KEY CLUSTERED ([Department_ID])
+)
+ON [PRIMARY]
+GO
